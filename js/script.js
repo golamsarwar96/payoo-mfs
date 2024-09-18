478eb5719531fc1d0820cfaa2af656fa35ace38a
golamsarwar98 <golamsarwar1896@gmail.com>
2024-09-19 02:25:31 +0600

    added event listener

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('login').addEventListener('click',function(e){
+    //Prevent reloading
+    e.preventDefault();
+    
+    //Add mobile number
+    const mobile = document.getElementById('mobile').value;
+    console.log(mobile);
+
+})
+
