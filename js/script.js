3cca21dfc580d0e005896ce09ffc7a65782e847f
golamsarwar98 <golamsarwar1896@gmail.com>
2024-09-19 02:59:44 +0600

    added home, Balance, Add money and cashout button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+//Step-1 Make a event handler for login button
+
 const btn = document.getElementById('login').addEventListener('click',function(e){
     //Prevent reloading
     e.preventDefault();
-    
-    //Add mobile number
+
+    //Step-2 Add mobile number
     const mobile = document.getElementById('mobile').value;
-    console.log(mobile);
+
+    //Step-3 Add pin
+    const pin = document.getElementById('pin').value;
+
+    //Step-4 Validate phone and pin number
+    if(mobile === "01631624674" &&  pin === "1234"){
+        //Step-5 Allow user to use website
+        window.location.href="./home.html"
+    }
+    else{
+        console.log("Wrong pin and password");
+    }
 
 })
 
